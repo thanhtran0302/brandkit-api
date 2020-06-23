26606ca071f6361358a264538a839f00b41573fd
Thanh TRAN <ttranpro32@gmail.com>
2020-06-23 22:38:38 +0200

    SQL directory

---
+++ sql/users/create.sql
@@ -0,0 +1,11 @@
+create table users(
+  id                varchar(50) CONSTRAINT userkey PRIMARY KEY UNIQUE,
+  email             varchar(255) NOT NULL UNIQUE,
+  password          varchar(255) NOT NULL,
+  is_activated      boolean NOT NULL,
+  creation_date     timestamp NOT NULL,
+  activation_token  varchar(50) NOT NULL,
+  plan              varchar(255) NOT NULL,
+  firstname         varchar(100) NULL,
+  lastname          varchar(100) NULL
+);
